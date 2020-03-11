32f4922cf7616cf52d292f0c4238213f265d297b
1179850608@qq.com <1179850608@qq.com>
2020-03-11 22:52:32 +0800

    首页开发

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,25 +1,49 @@
 <template>
-  <div>
-    <el-button @click="logout">退出</el-button>
-  </div>
+  <el-container>
+    <el-header>
+      Header
+      <el-button @click="logout">退出</el-button>
+    </el-header>
+    <el-container>
+      <el-aside width="200px">Aside</el-aside>
+      <el-main>Main</el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
 export default {
-  name: 'Home',
+  name: "Home",
   data() {
-    return {
-
-    };
+    return {};
   },
   methods: {
     logout() {
       window.sessionStorage.clear();
-      this.$router.push('./login')
+      this.$router.push("./login");
     }
   }
 };
 </script>
 <style scoped>
-
+.el-header, .el-footer {
+    background-color: #B3C0D1;
+    color: #333;
+    text-align: center;
+    line-height: 60px;
+  }
+  
+  .el-aside {
+    background-color: #D3DCE6;
+    color: #333;
+    text-align: center;
+    line-height: 200px;
+  }
+  
+  .el-main {
+    background-color: #E9EEF3;
+    color: #333;
+    text-align: center;
+    line-height: 160px;
+  }
 </style>
--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,11 +1,15 @@
 import Vue from 'vue'
 import Element from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
-import { Form, FormItem, Input, Button, Message } from 'element-ui'
+import { Form, FormItem, Input, Button, Message, Container, Header, Aside, Main } from 'element-ui'
 
 Vue.use(Element)
 Vue.use(Form);
 Vue.use(FormItem)
 Vue.use(Input)
 Vue.use(Button)
+Vue.use(Container)
+Vue.use(Header)
+Vue.use(Aside)
+Vue.use(Main)
 Vue.prototype.$message = Message
